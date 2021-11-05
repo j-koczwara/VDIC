910d237b5882d75076ad87baec2974bfd6fddc6c
Koczwara Joanna <j.koczwara45@gmail.com>
2021-11-05 15:05:50 +0100

    lab3 init

---
+++ lab03/INCA_libs/run.lnx86.19.09.d/files.ts
@@ -0,0 +1,2 @@
+1635888956 /home/student/jkoczwara/VDIC/lab02/alu/mtm_Alu.vp
+1636036362 /home/student/jkoczwara/VDIC/lab02/alu_tb.sv
---
+++ lab03/INCA_libs_gui/run.lnx86.19.09.d/files.ts
@@ -0,0 +1,2 @@
+1635888956 /home/student/jkoczwara/VDIC/lab02/alu/mtm_Alu.vp
+1636035663 /home/student/jkoczwara/VDIC/lab02/alu_tb.sv
